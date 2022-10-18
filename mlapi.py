fb5e2cb689caaa141188a213208f13f22bf7ddf4
Nityanand Mathur <nityanandmathur@gmail.com>
2022-10-18 22:40:18 +0530

    initial commit

---
+++ mlapi.py
@@ -0,0 +1,22 @@
+import pickle
+
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class ScoringItem(BaseModel):
+    YearsAtCompany: float
+    EmployeeSatisfaction: float
+    Position: str
+    Salary: int
+
+with open('./rfmodel.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.post('/')
+async def scoring_endpoint(item:ScoringItem):
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+    yhat = model.predict(df)
+    return {"Predictions":int(yhat)}
